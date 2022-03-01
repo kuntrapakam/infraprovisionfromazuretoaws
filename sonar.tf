5110ffe094e9694724147fbf2e472d337b747a12
rohith <rohirohithky@outlook.com>
2022-03-01 11:55:25 +0530

    initial commit

---
+++ sonar.tf
@@ -0,0 +1,41 @@
+resource "aws_instance" "mySonarInstance" {
+      ami           = "ami-048ff3da02834afdc"
+
+      key_name = var.key_name
+      instance_type = "t2.micro"
+      security_groups= [ "security_sonar_group_2022"]
+      tags= {
+        Name = "sonar_instance"
+      }
+    }
+
+ resource "aws_security_group" "security_sonar_group_2022" {
+      name        = "security_sonar_group_2022"
+      description = "security group for Sonar"
+
+      ingress {
+        from_port   = 9000
+        to_port     = 9000
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+
+     ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+
+     # outbound from Sonar server
+      egress {
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+
+      tags= {
+        Name = "security_sonar"
+      }
+    }
